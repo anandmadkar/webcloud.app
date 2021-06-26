437a469395f37ee4ab7614f54169c4a546c5aa25
Admin <Admin@LAPTOP-MPQ5ER6D>
2021-06-27 00:58:41 +0530

    coderefactor

---
+++ src/test/java/webcloud/app/webcloud/app/ApplicationTests.java
@@ -0,0 +1,13 @@
+package webcloud.app.webcloud.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
